e85477bbdfe25023611390f677c0d6502eca0a54
paras <kungfupantex@gmail.com>
2026-03-06 06:32:32 +0800

    POSTTEST_2 YEEE

---
+++ POSTTEST_2/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+    Public judulBuku As String
+    Public genreBuku As String
+    Public index As Integer
+
+    Private Function cekKosong(teks As String) As Boolean
+        If teks = "" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub tampilkanDaftarBuku()
+        lstBuku.Items.Clear()
+        For i As Integer = 0 To jumlahBuku - 1
+            lstBuku.Items.Add((i + 1).ToString() & ". " & daftarBuku(i, 0) & " - " & daftarBuku(i, 1))
+        Next
+    End Sub
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        judulBuku = txtJudulTambah.Text
+        genreBuku = txtGenre.Text
+
+        If cekKosong(judulBuku) Or cekKosong(genreBuku) Then
+            MessageBox.Show("Judul dan Genre harus diisi.")
+            Exit Sub
+        End If
+
+        Module1.tambahBuku(judulBuku, genreBuku)
+        tampilkanDaftarBuku()
+
+        txtJudulTambah.Clear()
+        txtGenre.Clear()
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        Dim judulHapus As String = txtJudulHapus.Text
+        Dim index As Integer = -1
+
+        For i As Integer = 0 To jumlahBuku - 1
+            If daftarBuku(i, 0).ToLower = judulHapus.ToLower Then
+                index = i
+                Exit For
+            End If
+        Next
+
+        If index <> -1 Then
+            Module1.hapusBuku(index)
+            tampilkanDaftarBuku()
+        Else
+            MessageBox.Show("Buku tidak ditemukan.")
+        End If
+
+    End Sub
+End Class
---
+++ POSTTEST_2/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+    Public daftarBuku(49, 1) As String
+    Public jumlahBuku As Integer = 0
+
+    Public Sub tambahBuku(judul As String, genre As String)
+        If jumlahBuku < 50 Then
+            daftarBuku(jumlahBuku, 0) = judul
+            daftarBuku(jumlahBuku, 1) = genre
+            jumlahBuku += 1
+        Else
+            Console.WriteLine("Daftar buku sudah penuh.")
+        End If
+    End Sub
+
+    Public Sub tampilkanDaftarBuku()
+        For i As Integer = 0 To jumlahBuku - 1
+            Console.WriteLine((i + 1).ToString() & ". " & daftarBuku(i, 0) & " - " & daftarBuku(i, 1))
+        Next
+    End Sub
+
+    Public Sub hapusBuku(index As Integer)
+        If index >= 0 And index < jumlahBuku Then
+            For i As Integer = index To jumlahBuku - 2
+                daftarBuku(i, 0) = daftarBuku(i + 1, 0)
+                daftarBuku(i, 1) = daftarBuku(i + 1, 1)
+            Next
+            jumlahBuku -= 1
+        Else
+            Console.WriteLine("Index tidak valid.")
+        End If
+    End Sub
+End Module
